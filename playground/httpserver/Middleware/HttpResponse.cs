089418015c94c5e1fed428d218768836592572df
cyberblob <paragon56@hotmail.com>
2020-08-21 20:51:10 -0500

    Add files via upload

--- playground/httpserver/Middleware/HttpResponse.cs
+++ playground/httpserver/Middleware/HttpResponse.cs
@@ -9,19 +9,24 @@ namespace Bytewizer.TinyCLR.WebServer
 {
     public class HttpResponse : Middleware
     {
+        int clientRequestCount = 1;
         protected override void Invoke(HttpContext context, RequestDelegate next)
         {
             try
             {
-                DebugHeaders(context);
+                //DebugHeaders(context);
 
-                string response = "<doctype !html><html><head><meta http-equiv='refresh' content='5'><title>Hello, world!</title>" +
-                                  "<style>body { background-color: #111 } h1 { font-size:2cm; text-align: center; color: white;}</style></head>" +
-                                  "<body><h1>" + DateTime.Now.Ticks.ToString() + "</h1></body></html>";
+                string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n" +
+                                  "<doctype !html><html><head><meta http-equiv='refresh' content='50'><title>Hello, world!</title>" +
+                                  "<style>body { background-color: #111 }" +
+                                  "h1 { font-size:2cm; text-align: center; color: white;}</style></head>" +
+                                  "<body><h1>" + DateTime.Now.ToString() + "</h1>" +
+                                  "<p style = 'color:red'> Client request count: " + clientRequestCount + "</body></html>";
 
                 context.Response.Write(response);
 
                 next(context);
+                clientRequestCount = CountN(clientRequestCount);
             }
             catch (Exception ex)
             {
@@ -38,6 +43,11 @@ namespace Bytewizer.TinyCLR.WebServer
             {
                 Debug.WriteLine($"{header.Key}={header.Value}");
             }
+        }
+        private static int CountN(int number)
+        {
+            ++number;
+            return number;
         }
     }
 }
