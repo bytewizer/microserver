6831b5b7342a6e0b1ce12014290eea7e629e8a2e
bytewizer <bytewizer@gmail.com>
2021-07-29 07:22:13 -1000

    Cleaned up project files

--- src/Bytewizer.TinyCLR.Http.Json/Properties/AssemblyInfo.cs
+++ src/Bytewizer.TinyCLR.Http.Json/Properties/AssemblyInfo.cs
@@ -5,12 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bytewizer Microserver")]
-[assembly: AssemblyDescription("HttpServer is a modular web server built for TinyCLR OS.")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bytewizer Http Server")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2dd3d5ab-a849-4398-c6a9-1a34635955b2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
--- src/Bytewizer.TinyCLR.Sockets/Properties/AssemblyInfo.cs
+++ src/Bytewizer.TinyCLR.Sockets/Properties/AssemblyInfo.cs
@@ -5,12 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bytewizer Microserver")]
-[assembly: AssemblyDescription("Microserver is a modular server built for IoT devices.")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("06e330d2-f947-4cca-8244-6117bfbf8ad5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
