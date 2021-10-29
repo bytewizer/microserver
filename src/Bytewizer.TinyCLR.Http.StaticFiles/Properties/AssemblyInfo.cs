aa8d042dd9d01aeeb6109b33343271a4b0ef24bb
microcompiler <58912473+bytewizer@users.noreply.github.com>
2021-10-28 17:55:16 -0700

    Update build files

--- src/Bytewizer.TinyCLR.Http.StaticFiles/Properties/AssemblyInfo.cs
+++ src/Bytewizer.TinyCLR.Http.StaticFiles/Properties/AssemblyInfo.cs
@@ -1,20 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("048913f0-07e4-4cd5-bcfc-74ae37b01d74")]
--- src/Bytewizer.TinyCLR.Sntp/Properties/AssemblyInfo.cs
+++ src/Bytewizer.TinyCLR.Sntp/Properties/AssemblyInfo.cs
@@ -1,20 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("674e936b-b2f3-4aa2-5b2d-301de786dd23")]
